73caf03c8cc5490da38ad09dc593d7a4e523e6cb
aavazquezs <aavazquezs@gmail.com>
2023-10-11 05:10:38 -0400

    removing innecesary methods

--- src/main/java/com/avangenio/warehouse/controller/SectionController.java
+++ src/main/java/com/avangenio/warehouse/controller/SectionController.java
@@ -4,17 +4,14 @@ import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.avangenio.warehouse.model.Product;
-import com.avangenio.warehouse.model.Section;
+
 import com.avangenio.warehouse.service.SectionService;
 
 @RestController
